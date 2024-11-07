130993181c9ef1c75795e3b5daa9892248dc13ec
master <master@SMLAB306PC12_25>
2024-11-06 20:41:37 -0500

    Actualizado

--- gjimenezS6B/Views/vEstudiante.xaml.cs
+++ gjimenezS6B/Views/vEstudiante.xaml.cs
@@ -1,9 +1,26 @@
+using gjimenezS6B.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
 namespace gjimenezS6B.Views;
 
 public partial class vEstudiante : ContentPage
 {
-	public vEstudiante()
+	private const string Url = "http://192.168.17.49/wsestudiantes/estudiante.php";
+	private readonly HttpClient cliente = new HttpClient();
+	private ObservableCollection<Models.Estudiante> estud;
+
+    public vEstudiante()
 	{
 		InitializeComponent();
+		Listar();
+	}
+
+	public async void Listar()
+	{
+		var content = await cliente.GetStringAsync(Url);
+		List<Models.Estudiante> listEst = JsonConvert.DeserializeObject<List<Estudiante>>(content);
+		estud = new ObservableCollection<Models.Estudiante>(listEst);
+		lvEstudiantes.ItemsSource = estud;
 	}
 }
