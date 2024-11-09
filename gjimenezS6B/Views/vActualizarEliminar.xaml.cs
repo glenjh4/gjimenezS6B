28485257e656d5a72d0bd544d277069e4419bb73
master <master@SMLAB306PC12_25>
2024-11-08 20:43:21 -0500

    FINAL

--- gjimenezS6B/Views/vActualizarEliminar.xaml.cs
+++ gjimenezS6B/Views/vActualizarEliminar.xaml.cs
@@ -1,9 +1,25 @@
+using gjimenezS6B.Models;
+
 namespace gjimenezS6B.Views;
 
 public partial class vActualizarEliminar : ContentPage
 {
-	public vActualizarEliminar()
+	public vActualizarEliminar(Estudiante datos)
 	{
 		InitializeComponent();
+		txtCodigo.Text= datos.codigo.ToString();
+		txtNombre.Text= datos.nombre.ToString();
+		txtApellido.Text= datos.apellido.ToString();
+		txtEdad.Text= datos.edad.ToString();
 	}
+
+    private void btnActualizar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnEliminar_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
